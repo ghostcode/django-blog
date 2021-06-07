330e82e43c965dee057e93421dd1d04da5a0610d
zhuxy <ghostcode521@gmail.com>
2021-06-07 21:27:52 +0800

    no message

--- blogproject/blogproject/settings/prod.py
+++ blogproject/blogproject/settings/prod.py
@@ -1,2 +1,19 @@
 from .base import *
 DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'USER': 'root',
+        'PASSWORD': 'Yuncan@1602',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
