66b8bc86304e2faed701884a33b19af538642395
zhuxy <ghostcode521@gmail.com>
2021-06-08 09:58:45 +0800

    no message

--- blogproject/blogproject/settings/prod.py
+++ blogproject/blogproject/settings/prod.py
@@ -4,6 +4,8 @@ DEBUG = False
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'miniapp.zhuyinze.com']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
